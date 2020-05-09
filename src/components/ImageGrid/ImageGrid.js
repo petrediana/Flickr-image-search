4d1c38216cce77964907a38d931cad185980241d
Petre Diana <petre.diana98@yahoo.ro>
2020-05-09 23:03:51 +0300

    rearenged a bit

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -5,21 +5,25 @@ import '../material.css';
 
 class ImageGrid extends Component {
   getImageCells() {
-    const images = this.props.images.filter(el => typeof el === 'string').map((image, index) => (
-      <ImageCell
-        className="elevation-2 round-corners-05"
-        src={image}
-        key={index}
-      />
-    ));
+    const images = this.props.images
+      .filter((el) => typeof el === "string")
+      .map((image, index) => (
+        <ImageCell
+          className="elevation-2 round-corners-05"
+          src={image}
+          key={index}
+        />
+      ));
 
     return images;
   }
-  
+
   render() {
     return (
-      <div className="image-grid-container elevation-1 round-corners-025">
-        { this.getImageCells() }
+      <div
+        className={`${this.props.className} image-grid-container elevation-1 round-corners-025`}
+      >
+        {this.getImageCells()}
       </div>
     );
   }
