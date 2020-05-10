419c0228a5ecbd355f5461a2f6a6679b0ea1863f
Petre Diana <petre.diana98@yahoo.ro>
2020-05-10 23:29:42 +0300

    forgot some imports again...

--- src/__tests__/components/MainMenu.test.js
+++ src/__tests__/components/MainMenu.test.js
@@ -3,12 +3,16 @@ import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
 
+import MainMenu from '../../components/MainMenu';
+import ImageGrid from '../../components/ImageGrid/ImageGrid';
+import Navbar from '../../components/Navbar/Navbar';
+
 configure({ adapter: new Adapter() });
 
-describe("MainMenu", () => {
+describe('<MainMenu />', () => {
     it(`sets the <ImageGrid> component's "images" attribute to the component's "pictures" prop`, () => {
         const component = mount(<MainMenu />);
-        const pictures = Array(10).fill("");
+        const pictures = Array(10).fill('');
 
         component.setState({ pictures });
 
