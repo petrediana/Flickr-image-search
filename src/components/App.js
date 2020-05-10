a27ad8c01241e92c7e6750867041a93b6bc312d8
Petre Diana <petre.diana98@yahoo.ro>
2020-05-10 21:13:36 +0300

    deleted unused import

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import config from '../constants/config';
 import MainMenu from '../components/MainMenu';
 
 class App extends Component {
   render() {
     return (
       <div style={{ backgroundColor: "#eee" }}>
-          App started
           <MainMenu/>
       </div>
     );
