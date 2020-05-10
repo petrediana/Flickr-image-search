36c421b08f72943db13a602042a0f01de41d2e90
Petre Diana <petre.diana98@yahoo.ro>
2020-05-10 18:11:20 +0300

    removed filter

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -6,7 +6,6 @@ import '../material.css';
 class ImageGrid extends Component {
   getImageCells() {
     const images = this.props.images
-      .filter((el) => typeof el === "string")
       .map((image, index) => (
         <ImageCell
           className="elevation-2 round-corners-05"
