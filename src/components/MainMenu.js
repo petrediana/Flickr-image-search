30348ab41a12bb8b64b890ac140e549e0a58493e
Petre Diana <petre.diana98@yahoo.ro>
2020-05-10 21:19:39 +0300

    rearrange methods and added logic for navbar selector

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,94 +1,101 @@
 import React, { Component } from 'react';
-import PhotoStore from '../store/PhotoStore';
-import  ImageGrid from './ImageGrid/ImageGrid';
-import  Navbar  from './Navbar/Navbar';
+import ImageGrid from './ImageGrid/ImageGrid';
+import Navbar from './Navbar/Navbar';
 import './MainMenu.css';
+import getFetchPromise from '../lib/picturesRetriever';
 
 class MainMenu extends Component {
     constructor() {
         super();
 
-        this.state = {
-            pictures: [],
-            pictureFilter: '',
-            isExecuted: false,
-            currentPage: 2
-        };
+    this.state = {
+      pictures: [],
+      pictureFilter: "",
+      isExecuted: false,
+      currentPage: 2,
+    };
 
-        this.photoStore = new PhotoStore();
+    this.checkIfInputIsNotNull = (input) => {
+      return input.trim().length > 0 ? true : false;
+    };
 
-        this.checkIfInputIsNotNull = (input) => {
-            return input.trim().length > 0 ? true : false;
-        };
+    this.setTargetedValue = (targetName, targetValue) => {
+      this.setState({
+        [targetName]: targetValue,
+      });
+    };
 
-        this.setTargetedValue = (targetName, targetValue) => {
-            this.setState({
-                [targetName]: targetValue
+    this.setPictures = (value) => {
+      this.setState({
+        pictures: value,
+      });
+    };
+
+    this.increaseCurrentPage = () => {
+      this.setState({
+        currentPage: this.state.currentPage + 1,
+      });
+    };
+
+    this.setIsExecuted = (value) => {
+      this.setState({
+        isExecuted: value,
+      });
+    };
+
+    this.applySearchAfterUserInput = (userInput, sortBy) => {
+      getFetchPromise(userInput, sortBy).then((pictures) => {
+          this.setPictures(pictures);
+      });
+    };
+
+    this.handleSearchInputChange = (evt) => {
+      if (this.checkIfInputIsNotNull(evt.target.value)) {
+        this.setTargetedValue(evt.target.name, evt.target.value);
+        this.applySearchAfterUserInput(evt.target.value, this.state.sortBy);
+      } else {
+        this.setPictures([]);
+      }
+    };
+
+    this.handleScroll = async () => {
+      if (
+        window.scrollY > document.body.offsetHeight - window.outerHeight &&
+        !this.state.isExecuted
+      ) {
+        this.setIsExecuted(true);
+
+        await getFetchPromise(this.state.pictureFilter, this.state.currentPage)
+            .then((pictures) => {
+                const result = this.state.pictures.concat(pictures);
+                this.setPictures(result);
             });
-        };
+        this.increaseCurrentPage();
+        this.setIsExecuted(false);
+      }
+    };
 
-        this.setPictures = (value) => {
-            this.setState({
-                pictures: value
-            });
-        };
-
-        this.increaseCurrentPage = () => {
-            this.setState({
-                currentPage: this.state.currentPage + 1
-            });
-        };
-
-        this.setIsExecuted = (value) => {
-            this.setState({
-                isExecuted: value
-            });
-        };
-
-        this.applySearchAfterUserInput = (searchInput) => {
-            this.photoStore.getPicturesThatMatchUsersInput(searchInput, 1);                
-        };
-
-        this.handleChange = (evt) => {
-            if (this.checkIfInputIsNotNull(evt.target.value)) {
-                this.setTargetedValue(evt.target.name, evt.target.value);
-                this.applySearchAfterUserInput(evt.target.value);                
-            } else {
-                this.setPictures([]);
-            }
-        };
-
-        this.handleScroll = async () => {
-            if (window.scrollY > (document.body.offsetHeight - window.outerHeight) && !this.state.isExecuted) {
-                this.setIsExecuted(true);
-                
-                await this.photoStore.getPicturesThatMatchUsersInput(this.state.pictureFilter, this.state.currentPage);
-                this.increaseCurrentPage();
-
-                setTimeout(() => {
-                    this.setIsExecuted(false);
-                }, 1000);
-            }    
-        };
+    this.handleSortByOptionChange = (value) => {
+        console.log(value, this.state.pictureFilter);
+      this.setTargetedValue('sortBy', value);
+      if (this.state.pictureFilter && this.state.pictureFilter !== '') {
+          this.applySearchAfterUserInput(this.state.pictureFilter, value);
+      }
     }
+  }
 
-    componentDidMount() {
-        this.photoStore.emitter.addListener('GET_USERS_PICTURES_SUCCESS', () => {
-            this.setPictures(this.photoStore.picturesArr);
-          }
-        );
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-        window.addEventListener('scroll', this.handleScroll);
-    }
-
-    render() {
-        return (
-            <div>
-                <Navbar onSearchInputChange={this.handleChange} />
-                <ImageGrid className="image-grid" images={this.state.pictures}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar onSearchInputChange={this.handleSearchInputChange} onSortByOptionChange={this.handleSortByOptionChange} />
+        <ImageGrid className="image-grid" images={this.state.pictures} />
+      </div>
+    );
+  }
 }
 
 export default MainMenu;
