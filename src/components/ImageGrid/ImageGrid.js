9efd37ac9527453f9128f34a5ea790cd36c0aad1
Petre Diana <petre.diana98@yahoo.ro>
2020-05-10 23:35:33 +0300

    added the boxed cat to the looks! :D

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import ImageCell from '../ImageCell/ImageCell';
 import './ImageGrid.css';
 import '../material.css';
+import empty from '../../res/empty.png';
 
 class ImageGrid extends Component {
+    render() {
+        return (
+            <div
+                className={`${this.props.className} image-grid-container elevation-1 round-corners-025`}
+            >
+                {this.getContent()}
+            </div>
+        );
+    }
+
+    getContent() {
+        return this.props.images.length > 0
+            ? this.getImageCells()
+            : this.getEmptyPlaceholder();
+    }
+
     getImageCells() {
         const images = this.props.images.map((image, index) => (
             <ImageCell
@@ -16,12 +33,15 @@ class ImageGrid extends Component {
         return images;
     }
 
-    render() {
+    getEmptyPlaceholder() {
         return (
-            <div
-                className={`${this.props.className} image-grid-container elevation-1 round-corners-025`}
-            >
-                {this.getImageCells()}
+            <div>
+                <img
+                    className="empty-placeholder"
+                    alt="empty content"
+                    src={empty}
+                ></img>
+                <h2>This box is empty!</h2>
             </div>
         );
     }
