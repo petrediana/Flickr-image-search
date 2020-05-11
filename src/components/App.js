8cc7a0f7a576d61949631c1128077ed0e50416ad
Petre Diana <petre.diana98@yahoo.ro>
2020-05-11 16:04:11 +0300

    changed import name

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import MainMenu from '../components/MainMenu';
-
+import Main from './Main';
 class App extends Component {
     render() {
         return (
             <div>
-                <MainMenu />
+                <Main />
             </div>
         );
     }
